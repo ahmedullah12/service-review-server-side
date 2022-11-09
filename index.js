bdeb36d6587363064bf77d1450759e595ff16c40
ahmedullah12 <gulamjoy321@gmail.com>
2022-11-09 17:15:35 +0600

    single service data sent by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
@@ -22,19 +22,25 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run () {
     try{
         const serviceCollection = client.db('ahmedPhotography').collection('services')
-        app.get('/services', async(req, res) => {
-            const query = {};
-            const cursor  = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            
-            res.send(services);
-        })
         app.get('/limited-services', async(req, res) => {
             const query = {};
-            const cursor  = serviceCollection.find(query);
+            const cursor  = serviceCollection.find(query).sort({_id: -1});
             const limitedServices = await cursor.limit(3).toArray();
             res.send(limitedServices);
+        });
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor  = serviceCollection.find(query).sort({_id: -1});
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
+
     }
     finally{
 
