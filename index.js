b5328d6a45c63b388ed2652e7e2b55c37ff71d3c
ahmedullah12 <gulamjoy321@gmail.com>
2022-11-09 21:04:11 +0600

    new service add api created

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run () {
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.post('/services', async(req, res) => {
+            const service = req.body;
+            console.log(service)
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
         app.get('/services/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)}
